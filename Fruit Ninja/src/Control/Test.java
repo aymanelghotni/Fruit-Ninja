2bea7622cb744ab4683cbc344c1541037dbd5ddb
aymanelghotni <48604489+aymanelghotni@users.noreply.github.com>
2019-05-22 04:54:03 +0200

    Last Commit...

--- Fruit Ninja/src/Control/Test.java
+++ Fruit Ninja/src/Control/Test.java
@@ -3,19 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import GUI.Game;
+
 public class Test {
 	public static void main(String[] args)
 	{
-		GameController game= new GameController(0);
-		game.randomizeGameObjects();
-		Command save=new SaveGame(game);
-		/*game.slice(game.objects.get(0));
-		game.slice(game.objects.get(1));
-		game.slice(game.objects.get(2));
-		game.slice(game.objects.get(3));
-		game.slice(game.objects.get(4));
-		System.out.println(game.player.getScore()+" "+game.player.getLives());*/
 		
+		Game.main(null);
 		
 		
 		
